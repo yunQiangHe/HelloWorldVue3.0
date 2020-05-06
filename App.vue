ae5983fca941adfff8f404303c24a780e53e7225
何云强 <heyunqiang@heyunqiangdeMacBook-Pro.local>
2020-05-06 23:46:52 +0800

    vue3.0 APi

--- App.vue
+++ App.vue
@@ -1,15 +1,29 @@
 <template>
   <section>
     <h2>{{ title }}</h2>
+    <User :userName="'虎克小哥哥'"></User>
+    <h3>我是：{{userInfo.userName}}</h3>
   </section>
 </template>
 
 <script>
+// @ is an alias to /src
+import User from "./components/User.vue";
+import { reactive, ref } from "vue";
+
 export default {
+  // setup() 函数是 vue3 中统一的入口函数,所有生命周期函数定义都是需要定义在次函数下才生效，
+  // setup 函数中无法访问到 this，所有的组件参数都是通过，setup函数中包含props和context参数，props参数默认是响应式的
   setup() {
+    const userInfo = reactive({ userName: "虎克小哥哥" });
+
     return {
-      title: "Hello World Vue3.0"
+      title: "Hello World Vue3.0",
+      userInfo
     };
+  },
+  components: {
+    User
   }
 };
 </script>
---
+++ components/User.vue
@@ -0,0 +1,65 @@
+<template>
+  <section>
+    <h2>我是:{{ userName }}</h2>
+  </section>
+</template>
+
+<script>
+import {
+  onBeforeMount,
+  onMounted,
+  onBeforeUpdate,
+  onUpdated,
+  onBeforeUnmount,
+  onUnmounted,
+  onErrorCaptured,
+  onRenderTracked,
+  onRenderTriggered
+} from "vue";
+export default {
+  name: "User",
+  props: {
+    userName: String
+  },
+  setup(props, context) {
+    console.log("setup 组件入口函数");
+    // context.attrs
+    // context.slots
+    // context.parent
+    // context.root
+    // context.emit
+    // context.refs
+    console.log(`组件传递props${props.userName}`);
+    console.log(`组件的上下文对象${context}`);
+
+    onRenderTracked(() => {
+      console.log("onRenderTracked");
+    });
+    onRenderTriggered(event => {
+      console.log("onRenderTriggered");
+    });
+    onBeforeMount(() => {
+      console.log("onBeforeMount");
+    });
+    onMounted(() => {
+      console.log("onMounted");
+    });
+    onBeforeUpdate(() => {
+      console.log("onBeforeUpdate");
+    });
+    onUpdated(() => {
+      console.log("onUpdated");
+    });
+    onBeforeUnmount(() => {
+      console.log("beforeDestroy");
+    });
+    onUnmounted(() => {
+      console.log("onUnmounted");
+    });
+    onErrorCaptured(() => {
+      console.log("onErrorCaptured");
+    });
+    return {};
+  }
+};
+</script>
